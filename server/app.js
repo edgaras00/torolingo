1cdb3416dafbdfbf11376a10d7f537b6b752f5ac
Edgar <edgaras993@gmail.com>
2023-04-24 13:41:19 -0400

    Implemented rate limiter for /user requests

--- server/app.js
+++ server/app.js
@@ -6,17 +6,27 @@ const problemRouter = require("./api/routes/problems");
 const vocabularyRouter = require("./api/routes/vocabulary");
 const userRouter = require("./api/routes/user");
 const errorController = require("./api/controllers/errorController");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
 
+// Rate limiter to limit /user requests
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message:
+    "Too many requests from this IP address. Please try again in an hour.",
+});
+
+app.use("/api/user", limiter);
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/problems", problemRouter);
-app.use("/vocab", vocabularyRouter);
-app.use("/user", userRouter);
+app.use("/api/problems", problemRouter);
+app.use("/api/vocab", vocabularyRouter);
+app.use("/api/user", userRouter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));

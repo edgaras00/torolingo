0575700fa7cfc53177e152c46f773d7ad9e5a6cd
Edgar <edgaras993@gmail.com>
2023-04-24 14:31:35 -0400

    data sanitization to prevent NoSQL query injection and XSS

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ const userRouter = require("./api/routes/user");
 const errorController = require("./api/controllers/errorController");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 require("dotenv").config();
 
 const app = express();
@@ -30,6 +32,12 @@ app.use(cors());
 // Body parser (body --> req.body)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS-attacks
+app.use(xss());
 
 app.use("/api/problems", problemRouter);
 app.use("/api/vocab", vocabularyRouter);
